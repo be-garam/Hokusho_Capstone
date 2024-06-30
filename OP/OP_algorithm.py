eaeb296ec78173edcff30b4c92c0963692613c67
harin9 <83578728+harin9@users.noreply.github.com>
2024-07-01 01:12:07 +0900

    [UPDATE] OP_algorithm_FINAL 수정사항 반영

--- OP/OP_algorithm.py
+++ OP/OP_algorithm.py
@@ -49,17 +49,23 @@ def equip_analysis(data, order, sku, quantity, rank, tolerance=0.05, max_iter=10
                    exchange_ratio=0.1, OTP_ratio={'A': [0.5, 0.5],'B': [0.5, 0.5]}, SEP_ratio=[0.5, 0.5]):
     import pandas as pd
     try:
+        # A 랭크에 대한 예외 처리
+        a = int(set(OTP_ratio['A']) != {0})  # a=0인 경우, A 랭크가 0인 것.
+        if a == 0:
+            data = data[data[rank] != 'A']
+            OTP_ratio['A'] = [0 for i in range(len(OTP_ratio['B']))]
+
         # OTP와 SEP 데이터 분리
         OTP = data[data[rank] != 'C']
         SEP = data[data[rank] == 'C']
 
         # qty 저장
-        OTP_qty = int(OTP[quantity].sum() / len(OTP_ratio['A']))
+        OTP_qty = int(OTP[quantity].sum() / len(OTP_ratio['B']))
         SEP_qty = int(SEP[quantity].sum() / len(SEP_ratio))
 
         # OTP A와 B 분리
-        OTP_A = OTP[OTP['Rank'] == 'A']
-        OTP_B = OTP[OTP['Rank'] == 'B']
+        OTP_A = OTP[OTP[rank] == 'A']
+        OTP_B = OTP[OTP[rank] == 'B']
 
         # sku 기준 grouping
         OTP_A_grouped = pd.DataFrame(OTP_A.groupby(sku)[quantity].sum()).reset_index()
@@ -67,28 +73,30 @@ def equip_analysis(data, order, sku, quantity, rank, tolerance=0.05, max_iter=10
         SEP_grouped = pd.DataFrame(SEP.groupby(sku)[quantity].sum()).reset_index()
 
         # 비율에 따른 분할
-        splited_A = initial_split_sku(OTP_A_grouped, OTP_ratio['A'], quantity)
+        if a==0:
+            splited_A = [pd.DataFrame(columns=OTP_A_grouped.columns) for i in range(len(OTP_ratio['A']))]  # 빈 array의 리스트
+        else:
+            splited_A = initial_split_sku(OTP_A_grouped, OTP_ratio['A'], quantity)
         splited_B = initial_split_sku(OTP_B_grouped, OTP_ratio['B'], quantity)
         splited_C = initial_split_sku(SEP_grouped, SEP_ratio, quantity)
 
         # OTP에 대한 할당
-        final_A, final_B = quantity_adjust_OTP(splited_A, splited_B, quantity, sku, tolerance, max_iter, exchange_ratio)
+        final_A, final_B = quantity_adjust_OTP(splited_A, splited_B, quantity, sku, a, tolerance, max_iter, exchange_ratio)
 
         # SEP에 대한 할당
         final_C = quantity_adjust_SEP(splited_C, quantity, sku, tolerance, max_iter, exchange_ratio)
 
         # 패턴 라벨링
-        grouped_A = pattern_labelling(final_A, 'OTP', 'A', sku)
+        grouped_A = pattern_labelling(final_A, 'OTP', 'A', sku) # a=0인 경우 empty
         grouped_B = pattern_labelling(final_B, 'OTP', 'B', sku)
         grouped_C = pattern_labelling(final_C, 'SEP', 'C', sku)
 
         # merge
-        merged_A = data.merge(grouped_A, on=sku, how='left')
+        merged_A = data.merge(grouped_A, on=sku, how='left') # a=0인 경우 A 랭크가 없는 데이터
         merged_B = merged_A.merge(grouped_B, on=sku, how='left')
         merged_C = merged_B.merge(grouped_C, on=sku, how='left')
 
         merged_C['EQUIP'] = merged_C['OTP_A'].combine_first(merged_C['OTP_B']).combine_first(merged_C['SEP_C'])
-
         order_categories = merged_C.groupby(order)['EQUIP'].apply(lambda x: ''.join(sorted(set(x)))).reset_index()
         order_categories['EQUIP패턴'] = 'pattern' + (order_categories.groupby('EQUIP').ngroup() + 1).astype(str)
         final_DF = merged_C.merge(order_categories[[order, 'EQUIP패턴']], on=order)
@@ -116,7 +124,7 @@ def initial_split_sku(grouped, ratios, quantity):
     except:
         print('Alert: please check your data(initial_split_sku)')
 
-def quantity_adjust_OTP(splited_A, splited_B, quantity, sku, tolerance=0.05, max_iter=10 ** 5, exchange_ratio=0.1):
+def quantity_adjust_OTP(splited_A, splited_B, quantity, sku, a=1, tolerance=0.05, max_iter=10 ** 5, exchange_ratio=0.1):
     try:
         import pandas as pd
         import numpy as np
@@ -124,17 +132,21 @@ def quantity_adjust_OTP(splited_A, splited_B, quantity, sku, tolerance=0.05, max
         start = time.time()
 
         # qty 계산
-        A_qty_list = [i[quantity].sum() for i in splited_A]
+        if a:
+            A_qty_list = [i[quantity].sum() for i in splited_A]
+            A_sku_min = min([len(i[sku]) for i in splited_A])
+        else:
+            A_qty_list = [0 for i in splited_B]
+            A_sku_min = 0
         B_qty_list = [i[quantity].sum() for i in splited_B]
-        total_qty_list = [A_qty_list[i] + B_qty_list[i] for i in range(len(splited_A))]
+        total_qty_list = [A_qty_list[i] + B_qty_list[i] for i in range(len(splited_B))]
         ideal_qty = int(sum(total_qty_list) / len(total_qty_list))
-        A_sku_min = min([len(i[sku]) for i in splited_A])
         B_sku_min = min([len(i[sku]) for i in splited_B])
 
         iteration = 0
         print(f'Alert: Start iteration...')
 
-        while iteration <= max_iter:  # test 데이터 기준으로 다 도는 데 약 4분 소요.
+        while iteration <= max_iter:
             # max와 min 그룹의 인덱스 구하기
             min_idx = total_qty_list.index(min(total_qty_list))
             max_idx = total_qty_list.index(max(total_qty_list))
@@ -146,23 +158,26 @@ def quantity_adjust_OTP(splited_A, splited_B, quantity, sku, tolerance=0.05, max
             # qty값 조정
             if diff_min > tolerance or diff_max > tolerance:
                 # 교환 비율 지정: 10%로!
-                slice_min_A = int(A_sku_min * max(diff_min, diff_max))
-                slice_min_B = int(B_sku_min * max(diff_min, diff_max))
+                if a:
+                    slice_min_A = int(A_sku_min * max(diff_min, diff_max))
+                    slice_min_A = int(A_sku_min * exchange_ratio)
+                    splited_A[min_idx].iloc[-(slice_min_A + 1):-1], splited_A[max_idx].iloc[0:slice_min_A] = splited_A[max_idx].iloc[0:slice_min_A].copy(), splited_A[min_idx].iloc[-(slice_min_A + 1):-1].copy()
 
-                slice_min_A = int(A_sku_min * exchange_ratio)
+                slice_min_B = int(B_sku_min * max(diff_min, diff_max))
                 slice_min_B = int(B_sku_min * exchange_ratio)
-                # 교환
-                splited_A[min_idx].iloc[-(slice_min_A + 1):-1], splited_A[max_idx].iloc[0:slice_min_A] = splited_A[max_idx].iloc[0:slice_min_A].copy(), splited_A[min_idx].iloc[-(slice_min_A + 1):-1].copy()
                 splited_B[min_idx].iloc[-(slice_min_B + 1):-1], splited_B[max_idx].iloc[0:slice_min_B] = splited_B[max_idx].iloc[0:slice_min_B].copy(), splited_B[min_idx].iloc[-(slice_min_B + 1):-1].copy()
+
             else:
                 print(f"Alert: Ideally divided! (tolerance={tolerance * 100}%)\n")
                 break
 
             # 다음 반복 위한 처리
-            A_qty_list = [i[quantity].sum() for i in splited_A]
+            if a:
+                A_qty_list = [i[quantity].sum() for i in splited_A]
+                splited_A = [frag.sort_values(by=quantity, ascending=False) for frag in splited_A]
+
             B_qty_list = [i[quantity].sum() for i in splited_B]
-            total_qty_list = [A_qty_list[i] + B_qty_list[i] for i in range(len(splited_A))]
-            splited_A = [frag.sort_values(by=quantity, ascending=False) for frag in splited_A]
+            total_qty_list = [A_qty_list[i] + B_qty_list[i] for i in range(len(splited_B))]
             splited_B = [frag.sort_values(by=quantity, ascending=False) for frag in splited_B]
 
             iteration += 1
